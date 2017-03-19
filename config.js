817a36bef85ff8db40ff7de6d8eac3f3e4b1cf0e
Harry John <harry671003@gmail.com>
2017-03-19 23:22:47 +0530

    Trying to figure out what's wrong #5

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'fbToken': process.env.FB_TOKEN || 'uhnNWEF3d18JEcZj6llQ',
-    'fbPageAccesstoken': process.env.FB_PAGE_ACCESS_TOKEN || 'EAAD5hnKQdbcBACrdx6jGbwTgQrj74Tue0qZCXdkuB4ooKUYQ0bafuLpeXLackwwGRwsrWjoZCh83EZA9d8N4qpZCWnXAX9I0YHZAUXpDxkpZAvM2ZBpLnMDUpOUw3S5gRV4sOCnwFjjZCkZATw5VOYh0mHAL3MOdvCo8mhh3AKimZAkAZDZD',
+    'fbPageAccessToken': process.env.FB_PAGE_ACCESS_TOKEN || 'EAAD5hnKQdbcBACrdx6jGbwTgQrj74Tue0qZCXdkuB4ooKUYQ0bafuLpeXLackwwGRwsrWjoZCh83EZA9d8N4qpZCWnXAX9I0YHZAUXpDxkpZAvM2ZBpLnMDUpOUw3S5gRV4sOCnwFjjZCkZATw5VOYh0mHAL3MOdvCo8mhh3AKimZAkAZDZD',
     'port': process.env.PORT || 6710,
     'instrumentationKey': process.env.INSTRUMENTATION_KEY || 'd41d85a1-f084-4d00-a7a1-f4b2f1322b8a'
 };
