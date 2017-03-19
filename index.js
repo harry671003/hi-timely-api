ddd295ef054d1a558528b6a37218659f75394678
Harry John <harry671003@gmail.com>
2017-03-19 22:25:00 +0530

    Added body parser

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const config = require('./config');
 require('./app/routes')(app, config);
 
 app.get('/', function(req, res) {
     res.send("Hello");
-})
+});
 
 app.listen(config.port, () => {
   console.log('We are live on ' + config.port);
