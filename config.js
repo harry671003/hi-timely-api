b6f1dcaaf196df3c7977adf675efe3835b5261e9
Harry John <harry671003@gmail.com>
2017-03-19 22:18:29 +0530

    Adding code from fb start page

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    'fbToken': process.env.FB_TOKEN || "uhnNWEF3d18JEcZj6llQ" ,
-    'port': process.env.PORT || 6710
+    'fbToken': process.env.FB_TOKEN || 'uhnNWEF3d18JEcZj6llQ',
+    'fbPageAccesstoken': process.env.FB_PAGE_ACCESS_TOKEN || 'EAAD5hnKQdbcBACrdx6jGbwTgQrj74Tue0qZCXdkuB4ooKUYQ0bafuLpeXLackwwGRwsrWjoZCh83EZA9d8N4qpZCWnXAX9I0YHZAUXpDxkpZAvM2ZBpLnMDUpOUw3S5gRV4sOCnwFjjZCkZATw5VOYh0mHAL3MOdvCo8mhh3AKimZAkAZDZD',
+    'port': process.env.PORT || 6710,
 };
